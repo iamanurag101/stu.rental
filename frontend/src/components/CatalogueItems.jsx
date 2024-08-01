e7f288612e7037c0071e3dc3f94c891efc1232e9
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-01 20:10:59 +0530

    Catalogue

--- frontend/src/components/CatalogueItems.jsx
+++ frontend/src/components/CatalogueItems.jsx
@@ -4,6 +4,8 @@ import { FaArrowRight } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
 
 const CatalogueItems = ({ isHome = false }) => {
+    const itemsToDisplay = isHome ? 3 : 9;
+
     return (
         <section className="px-4 py-6 rounded-xl">
             <div className="container-xl lg:container m-auto">
@@ -11,25 +13,21 @@ const CatalogueItems = ({ isHome = false }) => {
                     <h2 className="text-3xl md:text-5xl font-body font-medium text-sr-black mb-6 px-4">
                         {isHome ? 'Popular Accommodation' : 'Catalogue Items'}
                     </h2>
-                    <Link to='/Catalogue' className='text-links flex items-center'>
-                        {isHome ? <span className='mr-2 font-body font-semibold hidden md:inline-block'>View All Listings</span> : ""}
-                        {isHome ? <FaArrowRight className="-rotate-45" /> : ""}
-                    </Link>
+                    {isHome && (
+                        <Link to='/Catalogue' className='text-links flex items-center'>
+                            <span className='mr-2 font-body font-semibold hidden md:inline-block'>View All Listings</span>
+                            <FaArrowRight className="-rotate-45" />
+                        </Link>
+                    )}
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-                    <CatalogueItem />
-                    <CatalogueItem />
-                    <CatalogueItem />
-                    <CatalogueItem />
-                    <CatalogueItem />
-                    <CatalogueItem />
-                    <CatalogueItem />
-                    <CatalogueItem />
-                    <CatalogueItem />
+                    {Array.from({ length: itemsToDisplay }, (_, index) => (
+                        <CatalogueItem key={index} />
+                    ))}
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default CatalogueItems;
