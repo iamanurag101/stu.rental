3511078ac2a4dc975e7e758abdf42bac62fe1773
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-18 22:13:17 +0530

    Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,37 @@
-import './layout.scss';
-import Navbar from './components/Navbar/Navbar';
 import HomePage from './routes/HomePage/HomePage';
+import ListPage from './routes/ListPage/ListPage';
+import Layout from './routes/Layout/Layout';
+import SinglePage from './routes/SinglePage/SinglePage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        }
+      ]
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <Navbar/>
-      <div className="content">
-        <HomePage/>
-      </div>
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
