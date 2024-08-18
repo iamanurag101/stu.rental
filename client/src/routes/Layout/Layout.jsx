0b1d007cf85bc0364c3c32663faa3313c0353d22
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-18 13:23:12 +0530

    Layout

---
+++ client/src/routes/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import './Layout.scss';
+
+const layout = () => {
+  return (
+    <div className='layout'>
+        <Navbar/>
+        <div className="content">
+            <Outlet/>
+        </div>
+    </div>
+  )
+}
+
+export default layout
