70b06460a214ba8e9a596a214f388dfb1d789f5e
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-18 10:33:00 +0530

    Add files via upload

--- api/app.js
+++ api/app.js
@@ -1,8 +1,20 @@
-import express from "express";
+import express from "express"
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
 
-const app = express(); // App is created
+const app = express();
 
-// Specify Port Number to listen to
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
+
 app.listen(8800, ()=>{
-    console.log("Server is running");
+    console.log("Server is running!");
 })
