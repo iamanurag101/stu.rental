cd92edd42e245b5da50a4cc1a0133108300f0206
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-19 17:04:39 +0530

    Dummy Data

--- client/src/lib/dummyData.js
+++ client/src/lib/dummyData.js
@@ -1,6 +1,6 @@
 export const listData = [
     {
-      id: 3,
+      id: 1,
       title: "T7",
       img: "https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       bedroom: 1,
@@ -11,7 +11,7 @@ export const listData = [
       longitude: 88.4013458,
     },
     {
-      id: 4,
+      id: 2,
       title: "35-A",
       img: "https://images.pexels.com/photos/2467285/pexels-photo-2467285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       bedroom: 2,
@@ -22,7 +22,7 @@ export const listData = [
       longitude: 88.4366,
     },
     {
-      id: 5,
+      id: 3,
       title: "EC-280",
       img: "https://images.pexels.com/photos/276625/pexels-photo-276625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       bedroom: 3,
@@ -33,7 +33,7 @@ export const listData = [
       longitude: 88.36304,
     },
     {
-      id: 6,
+      id: 4,
       title: "1B",
       img: "https://images.pexels.com/photos/271816/pexels-photo-271816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       bedroom: 1,
@@ -47,24 +47,24 @@ export const listData = [
   
   export const singlePostData = {
     id: 1,
-    title: "Beautiful Apartment",
-    price: 1200,
+    title: "T7",
+    price: 800,
     images: [
       "https://images.pexels.com/photos/1918291/pexels-photo-1918291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       "https://images.pexels.com/photos/1428348/pexels-photo-1428348.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       "https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
       "https://images.pexels.com/photos/2467285/pexels-photo-2467285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     ],
-    bedRooms: 2,
+    bedRooms: 1,
     bathroom: 1,
     size: 861,
-    latitude: 51.5074,
-    longitude: -0.1278,
-    city: "London",
-    address: "1234 Broadway St",
-    school: "250m away",
-    bus: "100m away",
-    restaurant: "50m away",
+    latitude: 22.5063846,
+    longitude: 88.4013458,
+    city: "Kolkata",
+    address: "T7, Ruby Squre, Eastern Metropolitan Bypass",
+    hospital: "250m away",
+    busStop: "100m away",
+    pharmacy: "50m away",
     description:
       "Future alike hill pull picture swim magic chain seed engineer nest outer raise bound easy poetry gain loud weigh me recognize farmer bare danger. actually put square leg vessels earth engine matter key cup indeed body film century shut place environment were stage vertical roof bottom lady function breeze darkness beside tin view local breathe carbon swam declared magnet escape has from pile apart route coffee storm someone hold space use ahead sheep jungle closely natural attached part top grain your grade trade corn salmon trouble new bend most teacher range anybody every seat fifteen eventually",
   };
@@ -72,5 +72,6 @@ export const listData = [
   export const userData = {
     id: 1,
     name: "John Doe",
+    mail: "johndoe@gmail.com",
     img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
   };
