c0b13e37c39613d201b48d7902b681b5b25fa74d
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-20 23:22:16 +0530

    routes updated

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { getPosts, getPost, addPost, updatePost, deletePost } from "../controllers/post.controller.js";
+import {verifyToken} from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-    console.log("router works");
-});
 
-export default router
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", verifyToken, addPost);
+router.put("/:id", verifyToken, updatePost);
+router.delete("/:id", verifyToken, deletePost);
+
+export default router;
