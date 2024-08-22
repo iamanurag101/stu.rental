bf6be82335ea7a14938a177fb9dccce4bd44e136
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-22 09:19:13 +0530

    post controller

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,10 +1,9 @@
 import prisma from "../lib/prisma.js";
 
 export const getPosts = async (req, res) => {
-  const query = req.query;
-
   try {
-    const posts = await prisma.post.findMany()
+    const posts = await prisma.post.findMany();
+
     res.status(200).json(posts);
   } catch (err) {
     console.log(err);
@@ -33,19 +32,18 @@ export const getPost = async (req, res) => {
     res.status(500).json({ message: "Failed to get post" });
   }
 };
-
 export const addPost = async (req, res) => {
   const body = req.body;
   const tokenUserId = req.userId;
-  if (!body.postData) {
-    return res.status(400).json({ message: "Post data is required" });
-  }
 
   try {
     const newPost = await prisma.post.create({
       data: {
         ...body.postData,
         userId: tokenUserId,
+        postDetail:{
+          create: body.postDetail,
+        },
       },
     });
     res.status(200).json(newPost);
@@ -54,7 +52,6 @@ export const addPost = async (req, res) => {
     res.status(500).json({ message: "Failed to create post" });
   }
 };
-
 export const updatePost = async (req, res) => {
   try {
     res.status(200).json();
