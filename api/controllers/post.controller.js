3a2bc3e9e59960e007d2388fb2cb5e0b8a9c1f9c
Ankan Das <140325464+evuedeveloper@users.noreply.github.com>
2024-08-23 21:06:21 +0530

    maps rendering on search

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,10 +1,24 @@
 import prisma from "../lib/prisma.js";
 
 export const getPosts = async (req, res) => {
+  const query = req.query;
   try {
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+      where: {
+        gender: query.gender || undefined, 
+        city: query.city || undefined,
+        type: query.type || undefined,
+        property: query.property || undefined,
+        bedroom: parseInt(query.bedroom) || undefined,
+        price: {
+          gte: parseInt(query.minPrice) || undefined,
+          lte: parseInt(query.maxPrice) || undefined,
+        },
+      },
+    });
 
     res.status(200).json(posts);
+
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get posts" });
