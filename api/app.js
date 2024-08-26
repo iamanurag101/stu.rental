96a6ff05d40d156dac682fce48df528f62795e6b
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-26 11:47:18 +0530

    CORS issue fix

--- api/app.js
+++ api/app.js
@@ -8,7 +8,7 @@ import userRoute from "./routes/user.route.js"
 
 const app = express();
 
-app.use(cors())
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 
