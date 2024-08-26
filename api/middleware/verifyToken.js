61719881efc5b09c2902e0e03e1026b597a2e663
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-26 18:05:31 +0530

    Debugging

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken"
 
 export const verifyToken = (req,res,next) => {
     const token = req.cookies.token;
+    console.log(token);
+    
     if(!token) return res.status(401).json({ message: "Not Authenticated!" });
 
     jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, payload) => {
