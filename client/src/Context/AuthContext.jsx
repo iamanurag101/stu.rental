f25d6daae4cd915fc050a4b3bbb878a249e9a679
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-26 19:04:42 +0530

    Auth Context

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -1,3 +1,7 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
 export const AuthContextProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(
         JSON.parse(localStorage.getItem("user")) || null
