b365bdbf5ea26b9ba8639357ad61d2672a105265
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-26 18:46:29 +0530

    Map marker

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
 import { AuthContextProvider } from './Context/AuthContext.jsx'
+import 'leaflet/dist/leaflet.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
