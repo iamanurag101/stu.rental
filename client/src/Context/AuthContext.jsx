b7af1616d51fea56a9fa0fdc3ab3287811f2c6db
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-26 19:01:06 +0530

    Auth Context

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -1,23 +1,23 @@
-import { createContext, useEffect, useState } from "react";
-
-export const AuthContext = createContext();
-
 export const AuthContextProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState (
+    const [currentUser, setCurrentUser] = useState(
         JSON.parse(localStorage.getItem("user")) || null
     );
 
     const updateUser = (data) => {
         setCurrentUser(data);
+        localStorage.setItem("user", JSON.stringify(data)); // Updated in the same function
     };
 
-    useEffect(()=>{
-        localStorage.setItem("user", JSON.stringify(currentUser));
-    },[currentUser]);
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem("user"));
+        if (storedUser) {
+            setCurrentUser(storedUser);
+        }
+    }, []); // Empty dependency array
 
     return (
-        <AuthContext.Provider value={{ currentUser,updateUser }}>
+        <AuthContext.Provider value={{ currentUser, updateUser }}>
             {children}
         </AuthContext.Provider>
     );
-}
+};
