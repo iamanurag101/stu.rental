2dfb4d554646d3ce240c3333b2926f1fde913beb
Anurag Dey <iamanuragdey005@gmail.com>
2024-08-27 00:14:17 +0530

    Auth Controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -64,6 +64,7 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
+        sameSite: 'None',
         // secure:true,
         maxAge: age,
       })
