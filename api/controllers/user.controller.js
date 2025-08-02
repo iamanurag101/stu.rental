169b1a0806555a5d44db4be0d9b8a5d4d8d6d084
Anurag Dey <iamanuragdey005@gmail.com>
2025-08-02 19:07:41 +0530

    Disable getUsers Route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,15 +1,15 @@
 import prisma from "../lib/prisma.js";
 import bcrypt from "bcrypt";
 
-export const getUsers = async (req, res) => {
-    try {
-      const users = await prisma.user.findMany();
-      res.status(200).json(users);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ message: "Failed to get users!" });
-    }
-  };
+// export const getUsers = async (req, res) => {
+//     try {
+//       const users = await prisma.user.findMany();
+//       res.status(200).json(users);
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json({ message: "Failed to get users!" });
+//     }
+//   };
   
 export const getUser = async (req, res) => {
     const id = req.params.id;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -11,7 +11,7 @@ import {verifyToken} from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
+// router.get("/", getUsers);
 router.get("/search/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
